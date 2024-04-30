c44e5e91b8ee4de49bc6e0efb8d42b5a22282f0f
andresguepar <34109816+andresguepar@users.noreply.github.com>
2024-04-30 15:43:54 -0500

    Finished

--- POO/Main.py
+++ POO/Main.py
@@ -1,4 +1,62 @@
-from POO.User import User
+
+from poo.Payment import Payment
+from poo.PaymentService import PaymentService
+from poo.Product import Product
+from poo.ShoppingCart import ShoppingCart
+from poo.User import User
+from poo.UserService import UserService
+
 
 def main():
-    user = User("Pacho","pacho@email.com")
+    shoppingCart = ShoppingCart()
+    userService = UserService()
+    paymentService = PaymentService()
+    
+
+    while True:
+        user = User("Pacho","78132","pacho@email.com")
+        product = Product("ProductoPredeterminado", 1)
+        shoppingCart.add(product)
+        userService.Add(user)
+        print("\nOptions:")
+        print("1. Add Product")
+        print("2. Remove Product")
+        print("3. Edit Account")
+        print("4. Checkout")
+        print("5. Payment")
+        print("6. Exit")
+        choice = input("Select an option: ")
+
+        if choice == '1':
+            name = input("Name: ")
+            price = input("Price: ")
+            product2 = Product.Product(name,price)
+            shoppingCart.add(product2)
+
+        elif choice == '2':
+            shoppingCart.remove(product2)
+
+        elif choice == '3':
+            id = 0
+            name = input("Name: ")
+            phone = input("Phone: ")
+            email = input("Email: ")
+            user2 = User.User(name,phone,email)
+            userService.edit(id,user2)
+
+        elif choice == '4':
+            print(f"Your total price is: {shoppingCart.checkout(product)}")
+
+        elif choice == '5':
+            payment = Payment(user,product,shoppingCart.checkout(product))
+            paymentService.pay(payment)
+
+        elif choice == '6':
+            print("Exiting...")
+            break
+
+        else:
+            print("Invalid option.")
+
+if __name__ == "__main__":
+    main()
---
+++ POO/PaymentService.py
@@ -0,0 +1,8 @@
+
+
+from poo.Payment import Payment
+
+
+class PaymentService:
+    def pay(self,payment : Payment):
+        print(f"Payment succesful, here is your receipt: {payment.user.name} compró {payment.product.name} con el valor de {payment.product.price}.")
